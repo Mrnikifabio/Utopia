ab8076f0feef66ca0e39917a76583482d3617380
Fabio Crugnola <fabio.crugnola@student.supsi.ch>
2023-01-08 20:00:24 +0100

    How were tests supposed to run without assets?

--- engine/test/OVOFactoryTest.cpp
+++ engine/test/OVOFactoryTest.cpp
@@ -53,7 +53,7 @@ void OVOFactoryTest::nonExistentFileTest()
 void OVOFactoryTest::fromFileTest()
 {
 	using namespace utopia;
-	auto scene = OVOFactory::getInstance().fromFile("bin/Test/assets/simple3dScene.ovo");
+	auto scene = OVOFactory::getInstance().fromFile("assets/simple3dScene.ovo");
 
 	assert(!Utopia::getInstance().getMaterialByName("14 - Default").expired());
 	assert(!Utopia::getInstance().getMaterialByName("08 - Default").expired());
