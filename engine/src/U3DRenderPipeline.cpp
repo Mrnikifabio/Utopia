3bb7837d359f2e17907af20d94835256118b659b
Lorenzo Di Folco <lorenzo.difolco@student.supsi.ch>
2023-01-09 14:40:00 +0100

    URenderPipeline Refactor

--- engine/src/U3DRenderPipeline.cpp
+++ engine/src/U3DRenderPipeline.cpp
@@ -1,11 +1,13 @@
 #include "U3DRenderPipeline.h"
 #include "UCamera.h"
+#include "ULight.h"
 #include <vector>
 
 using namespace utopia;
 
 struct U3DRenderPipeline::pimpl {
 	std::vector<std::unique_ptr<U3DRenderNode>> m_nodes;
+	std::vector<std::unique_ptr<U3DRenderNode>> m_lights;
 };
 
 struct U3DRenderPipeline::U3DRenderNode {
@@ -25,13 +27,26 @@ U3DRenderPipeline::~U3DRenderPipeline() = default;
 
 void U3DRenderPipeline::pass(std::weak_ptr<UNode> node, const glm::mat4& mat)
 {
-	auto uRenderNode = std::unique_ptr<U3DRenderNode>(new U3DRenderNode(node, mat));
-	m_pimpl->m_nodes.push_back(std::move(uRenderNode));
+	if (dynamic_cast<ULight*>(node.lock().get()))
+	{
+		auto renderNode = std::unique_ptr<U3DRenderNode>(new U3DRenderNode(node, mat));
+		m_pimpl->m_lights.push_back(std::move(renderNode));
+	}
+	else
+	{
+		auto renderNode = std::unique_ptr<U3DRenderNode>(new U3DRenderNode(node, mat));
+		m_pimpl->m_nodes.push_back(std::move(renderNode));
+	}
 }
 
 void U3DRenderPipeline::render()
 {
 	for (auto& node : m_pimpl->m_nodes)
+	{
+		node->node.lock()->setModelView(node->mat);
+		node->node.lock()->render();
+	}
+	for (auto& node : m_pimpl->m_lights)
 	{
 		node->node.lock()->setModelView(node->mat);
 		node->node.lock()->render();
